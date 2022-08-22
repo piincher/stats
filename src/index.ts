8c79ab1b9c1b29c0471dabce8d56a4cd7b717fb6
Ibrahim <ikouma269@gmail.com>
2022-08-22 20:50:25 +0800

    extract date

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { MatchResult } from "./Matchresult";
+import { CsvFileReader } from "./CsvFileReader";
+const reader = new CsvFileReader("football.csv");
+reader.read();
+console.log(reader.data);
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`man united ${manUnitedWins}`);
